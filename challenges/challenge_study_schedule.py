367564554b1380296f8fef35984b3d9e86f73af3
Paula <paulasalinor@gmail.com>
2022-11-19 20:41:43 -0300

    feat: add study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+    if target_time == 0 or target_time is None:
+        return None
+
+    student_online = 0
+    for student_time in permanence_period:
+        if not isinstance(student_time[0], int) or not isinstance(
+            student_time[1], int
+        ):
+            return None
+        if student_time[0] <= target_time and student_time[1] >= target_time:
+            student_online = student_online + 1
+
+    return student_online
