9d4a9cd725fe86a918c83dc602a5b2adb1d2fb32
Paula <paulasalinor@gmail.com>
2022-11-19 20:23:36 -0300

    feat: add is_palindrome_recursive function

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+
+    if word[low_index] != word[high_index]:
+        return False
+    elif low_index >= high_index:
+        return True
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
