3321a4e03587087c30deb345b4fa8e22bb062a2c
Paula <paulasalinor@gmail.com>
2022-11-19 20:31:33 -0300

    feat: add is_anagram function

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,18 @@
+def compare(first_word, second_word):
+    for letter in first_word:
+        if letter in second_word:
+            second_word = second_word.replace(letter, "", 1)
+        else:
+            return False
+
+    return True
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(first_string) != len(second_string):
+        return False
+
+    if first_string == "" or second_string == "":
+        return False
+
+    return compare(first_string.lower(), second_string.lower())
