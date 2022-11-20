dae374424bf03acac083cacfca3f180c42328078
Paula <paulasalinor@gmail.com>
2022-11-19 21:06:19 -0300

    feat: add find_duplicate

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,11 +1,20 @@
-def is_anagram(first_string, second_string):
-    first_word = list(first_string.lower())
-    second_word = list(second_string.lower())
-    if len(first_word) != len(second_word):
-        return False
+def compare_word(first_word, second_word):
     for letter in first_word:
         if letter in second_word:
-            second_word.remove(letter)
+            second_word = second_word.replace(letter, "", 1)
         else:
             return False
+
     return True
+
+
+def is_anagram(first_string, second_string):
+    if len(first_string) != len(second_string):
+        return False
+
+    if first_string == "" or second_string == "":
+        return False
+
+    first_word, second_word = first_string.lower(), second_string.lower()
+
+    return compare_word(first_word, second_word)
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,20 @@
+def aux(arr):
+    if not arr or len(arr) < 1:
+        return False
+
+    arr.sort()
+    if type(arr[-1]) is not int:
+        return False
+
+    return arr
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if aux(nums):
+        for index, number in enumerate(nums[:-1]):
+            if number < 0:
+                return False
+            if number == nums[index + 1]:
+                return number
+
+    return False
