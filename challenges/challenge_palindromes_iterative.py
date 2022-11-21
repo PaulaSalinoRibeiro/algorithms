1900d8ac3bd4769a22031dca68881f9015d6499a
Paula <paulasalinor@gmail.com>
2022-11-21 13:31:26 -0300

    feat: is_palindrome_iterative fucntion

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,9 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+    result = True
+    for index, letter in enumerate(word[: len(word) // 2]):
+        reverse_index = -index - 1
+        if letter != word[reverse_index]:
+            result = False
+    return result
